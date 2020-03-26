cde98360a98728bb86b17a16880d6ae406229921
WoodTree37 <46052460+WoodTree37@users.noreply.github.com>
2020-03-26 15:13:04 +0100

    Requested help on PAM MAtura

--- scripts.js
+++ scripts.js
@@ -45,6 +45,18 @@
                 }
             }
 
+// PAM Matura
+function PAMMAHelp() {
+    var txt;
+    if (confirm("Press a button!")) {
+      txt = "You pressed OK!";
+    } else {
+      txt = "You pressed Cancel!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+  }
+
+
 
 //Cookies
 /*////////////////////////////////////////////////////////////////////////////////////////*/
