d844fd5ab29f0d725ddb804e9c8f11b11dfa7127
WoodTree37 <46052460+WoodTree37@users.noreply.github.com>
2020-12-27 22:24:28 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -100,15 +100,11 @@ var countDownDate = new Date("May 29, 2020 12:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
-  var distance = countDownDate + now;
+  var distance =  now - countDownDate;
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("timer").innerHTML = days + "days ";
 
 }, 1000);
 }
