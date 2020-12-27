9bf0e3176538411b54434a7816a144b10f51fd58
WoodTree37 <46052460+WoodTree37@users.noreply.github.com>
2020-12-27 22:06:38 +0100

    Updated timer on frontpage

--- scripts.js
+++ scripts.js
@@ -100,7 +100,7 @@ var countDownDate = new Date("May 29, 2020 12:00:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
-  var distance = countDownDate - now;
+  var distance = countDownDate + now;
 
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
